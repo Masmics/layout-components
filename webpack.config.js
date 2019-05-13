7395d8cb156511f764abe2455783cf68ef60a0c1
Masmics <cherlocker3113@gmail.com>
2019-05-13 11:36:10 -0700

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlPlugIn = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    //filename: './dist/bundle.js'
+    filename: './dist/bundle.[hash].js'
+  },
+  devServer: {
+    port: 7890
+  },
+  plugins: [
+    new HtmlPlugIn({ template: './src/index.html'})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')(),
+                require('postcss-nested')()
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      }
+    ]
+  }
+}
